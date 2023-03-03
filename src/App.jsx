bddf9ba70aea23f18f9f34ab20405af831d7020d
Andrea Mac <aleatrengrouse@gmail.com>
2023-03-03 17:06:32 +0000

    added and imported all elements from components to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import Header from "./components/header/Header";
+import Nav from "./components/navBar/Nav";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contacts from "./components/contacts/Contacts";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Portfolio />
+      <Contacts />
+      <Footer />
     </div>
   );
 };
