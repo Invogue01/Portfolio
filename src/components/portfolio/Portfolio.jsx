4831356987d3180d415fd475a4a16ef300d8f528
Andrea Mac <aleatrengrouse@gmail.com>
2023-05-24 13:20:06 +0100

    finished Projects section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import "./portfolio.css";
+import IMG1 from '../../assets/web1.png'
+import IMG2 from '../../assets/web2.png'
+import IMG3 from '../../assets/web3.gif'
+import IMG4 from '../../assets/web4.png'
+import IMG5 from '../../assets/web5.png'
+import IMG6 from '../../assets/web6.png'
 
 const Portfolio = () => {
   return <section id='portfolio'>
@@ -8,10 +14,64 @@ const Portfolio = () => {
 
 <div className="container portfolio__container">
 <article className='portfolio__item'>
-  <div className="portfolio__item-image"></div>
+  <div className="portfolio__item-image">
+   <img src={IMG1} alt="codeBuster landing page" />
+  </div>
   <h3>Title</h3>
-  <a href="https://github.com" className='btn'target='_blank'>Github</a>
+  <div className="portfolio__item-cta">
+  <a href="https://github.com" className='btn' target='_blank'>Github</a>
   <a href="https://github.com" className='btn btn-primary'target='_blank'>Demo</a>
+  </div>
+</article>
+<article className='portfolio__item'>
+  <div className="portfolio__item-image">
+   <img src={IMG2} alt="socialAPI landing page" />
+  </div>
+  <h3>Title</h3>
+  <div className="portfolio__item-cta">
+  <a href="https://github.com" className='btn' target='_blank'>Github</a>
+  <a href="https://github.com" className='btn btn-primary'target='_blank'>Demo</a>
+  </div>
+</article>
+<article className='portfolio__item'>
+  <div className="portfolio__item-image">
+   <img src={IMG3} alt="Note Taking website landing page" />
+  </div>
+  <h3>Title</h3>
+  <div className="portfolio__item-cta">
+  <a href="https://github.com" className='btn' target='_blank'>Github</a>
+  <a href="https://github.com" className='btn btn-primary'target='_blank'>Demo</a>
+  </div>
+</article>
+<article className='portfolio__item'>
+  <div className="portfolio__item-image">
+   <img src={IMG4} alt="employees server code picture" />
+  </div>
+  <h3>Title</h3>
+  <div className="portfolio__item-cta">
+  <a href="https://github.com" className='btn' target='_blank'>Github</a>
+  <a href="https://github.com" className='btn btn-primary'target='_blank'>Demo</a>
+  </div>
+</article>
+<article className='portfolio__item'>
+  <div className="portfolio__item-image">
+   <img src={IMG5} alt="readMe file generator app code picture" />
+  </div>
+  <h3>Title</h3>
+  <div className="portfolio__item-cta">
+  <a href="https://github.com" className='btn' target='_blank'>Github</a>
+  <a href="https://github.com" className='btn btn-primary'target='_blank'>Demo</a>
+  </div>
+</article>
+<article className='portfolio__item'>
+  <div className="portfolio__item-image">
+   <img src={IMG6} alt="weather API website landing page" />
+  </div>
+  <h3>Title</h3>
+  <div className="portfolio__item-cta">
+  <a href="https://github.com" className='btn' target='_blank'>Github</a>
+  <a href="https://github.com" className='btn btn-primary'target='_blank'>Demo</a>
+  </div>
 </article>
 
 </div>
