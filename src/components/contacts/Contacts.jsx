2d639a3747981dd3374fae41c1373e5a9c45bb6b
Andrea Mac <aleatrengrouse@gmail.com>
2023-07-26 12:16:16 +0100

    contacts container and form

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -2,7 +2,23 @@ import React from "react";
 import "./contact.css";
 
 const Contacts = () => {
-  return <section id='contact'>Contacts</section>;
+  return(
+    <section>
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact_options">
+          <article className="contact__option">
+            
+          </article>
+        </div>
+        {/*End of Contact Options*/}
+        <form action=""></form>
+
+      </div>
+    </section>
+  )
 };
 
 export default Contacts;
