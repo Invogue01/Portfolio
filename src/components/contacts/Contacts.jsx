6f2b721c695f7f6098f93cbd3f3b02419fec05b6
Andrea Mac <aleatrengrouse@gmail.com>
2023-07-26 12:33:18 +0100

    email icon needs fixing

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import "./contact.css";
+import {MdOutlineEmail} from "react-icons/md"
 
 const Contacts = () => {
   return(
-    <section>
+    <section id="contact">
       <h5>Get in Touch</h5>
       <h2>Contact Me</h2>
 
       <div className="container contact__container">
         <div className="contact_options">
           <article className="contact__option">
-            
+            <MdOutlineEmail/>
+            <h4>Email</h4>
+            <h5>aleatrengrouse@gmail.com</h5>
+            <a href="mailto:aleatrengrouse@gmail.com">Send a Message</a>
           </article>
         </div>
         {/*End of Contact Options*/}
